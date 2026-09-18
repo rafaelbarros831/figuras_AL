3180e91c252e817d389555511a8e65692ddf966e
rafaelbarros831 <rafasigma80@gmail.com>
2026-09-18 12:57:21 -0300

    Projeto Configurado

---
+++ projeto_configurado/projeto_configurado.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main(){
+
+    float tami_circ = 100.0f;
+
+    CircleShape circ(tami_circ);
+    circ.setPosition(Vector2f(100.0f, 100.0f));
+    circ.setFillColor(Color::Green);
+
+    RenderWindow janela(VideoMode({400,200}), "Teste:");
+
+    while(janela.isOpen() == true){
+        while (optional evento = janela.pollEvent()) {
+
+            if (evento ->is<Event::Closed>()) {
+                janela.close();
+            }
+        }
+
+        janela.clear(Color::Blue);
+
+        janela.draw(circ);
+
+        if(Keyboard::isKeyPressed(Keyboard::Key::Space) && tami_circ > 10.0f){
+			tami_circ -= 0.01f;
+			circ.setRadius(tami_circ);
+        }
+
+        janela.display();
+    }
+
+    return 0;
+}
+
