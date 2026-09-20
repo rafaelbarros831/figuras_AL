6b7e292158786357409112520a63828fbff43054
rafaelbarros831 <rafasigma80@gmail.com>
2026-09-20 13:08:35 -0300

    Lógica do círculo

--- projeto_configurado/projeto_configurado.cpp
+++ projeto_configurado/projeto_configurado.cpp
@@ -4,15 +4,29 @@
 using namespace sf;
 using namespace std;
 
-int main(){
+RenderWindow janela(VideoMode({ 400,200 }), "Teste:");
 
-    float tami_circ = 100.0f;
+bool Gs;
+float tami_circ = 100.0f;
+
+void GeraCirc() {
 
     CircleShape circ(tami_circ);
     circ.setPosition(Vector2f(100.0f, 100.0f));
     circ.setFillColor(Color::Green);
 
-    RenderWindow janela(VideoMode({400,200}), "Teste:");
+    janela.clear(Color::Blue);
+
+    janela.draw(circ);
+
+    if (Keyboard::isKeyPressed(Keyboard::Key::Space) && tami_circ > 10.0f) {
+        tami_circ -= 0.01f;
+        circ.setRadius(tami_circ);
+    }
+
+}
+
+int main(){
 
     while(janela.isOpen() == true){
         while (optional evento = janela.pollEvent()) {
@@ -24,11 +38,12 @@ int main(){
 
         janela.clear(Color::Blue);
 
-        janela.draw(circ);
+        if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
+            Gs = true;
+        }
 
-        if(Keyboard::isKeyPressed(Keyboard::Key::Space) && tami_circ > 10.0f){
-			tami_circ -= 0.01f;
-			circ.setRadius(tami_circ);
+        if (Gs != false) {
+            GeraCirc();
         }
 
         janela.display();
