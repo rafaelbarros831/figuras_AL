4803c8caf7d6feea3d63af228ebd5e188e193b75
Gabriel Marciano Arante de Assis <141376806+GabrielMarcianoarante@users.noreply.github.com>
2026-09-22 03:56:53 -0300

    Corrige reflexo que sumia e adiciona rotacao (matriz)

--- projeto_configurado/include/Transformacao.hpp
+++ projeto_configurado/include/Transformacao.hpp
@@ -6,4 +6,7 @@ class Transformacao
 {
 public:
     static Matriz escala(double fator);
+    static Matriz reflexaoX();
+    static Matriz reflexaoY();
+    static Matriz rotacao(double anguloGraus);
 };
--- projeto_configurado/src/Figura.cpp
+++ projeto_configurado/src/Figura.cpp
@@ -6,7 +6,7 @@ Figura::Figura()
 
 void Figura::addPonto(const Ponto& ponto)
 {
-    pontos.push_back(ponto);
+	pontos.push_back(ponto);
 }
 
 void Figura::aplicarMatriz(const Matriz& matriz)
@@ -15,4 +15,37 @@ void Figura::aplicarMatriz(const Matriz& matriz)
 	{
 		ponto = matriz.aplicar(ponto);
 	}
+}
+
+void Figura::transladar(double dx, double dy)
+{
+	for (auto& ponto : pontos)
+	{
+		ponto = Ponto(ponto.getX() + dx, ponto.getY() + dy);
+	}
+}
+
+Ponto Figura::calcularCentro() const
+{
+	double somaX = 0.0;
+	double somaY = 0.0;
+
+	for (const auto& ponto : pontos)
+	{
+		somaX += ponto.getX();
+		somaY += ponto.getY();
+	}
+
+	double quantidade = static_cast<double>(pontos.size());
+
+	return Ponto(somaX / quantidade, somaY / quantidade);
+}
+
+void Figura::aplicarMatrizCentrada(const Matriz& matriz)
+{
+	Ponto centro = calcularCentro();
+
+	transladar(-centro.getX(), -centro.getY());
+	aplicarMatriz(matriz);
+	transladar(centro.getX(), centro.getY());
 }
